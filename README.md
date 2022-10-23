cd52f188a988e623572c01abaeb7310322c78b33
Eddyconnect <106748705+Eddyconnect@users.noreply.github.com>
2022-10-23 19:02:33 +0100

    linear search algorithm

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# search_algorithim
+This is a linear and binary search algorithms
  
---
+++ linear_search.py
@@ -0,0 +1,27 @@
+def linear_search(list, target):
+    """
+    Returns the index position of a target in a list if found, else it returns None.
+    """
+
+    for i in range(0, len(list)):
+        if i == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not found in the list")
+
+"""
+Below is a small code example to test the above linear_search algorithim
+"""
+
+numb = [1,2,3,4,5,6,7,8,9,10]
+result = linear_search(numb, 12)
+verify(result)
+
+numb = [1,2,3,4,5,6,7,8,9,10]
+result = linear_search(numb, 6)
+verify(result)
